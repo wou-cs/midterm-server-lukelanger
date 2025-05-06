2cfb2fe0edcc45e682f615fd273dd01db2a7c1c5
Luke Langer <lukelanger@gmail.com>
2025-05-05 22:48:27 -0700

    Add API endpoint for calculations

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, request
-
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
+@app.route('/api/calcs/<value>', methods=['GET'])
+def calculate(value):
+    try:
+        num = int(value)
+        if num <= 0:
+            abort(404)
+    except ValueError:
+        abort(404)
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+    result = {
+        "dec": num - 1,
+        "hex": hex(num)
+    }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
